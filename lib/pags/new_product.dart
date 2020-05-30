512838bb7f422e2f3f482d09f95ed496a3867018
M.M.Hasibuzzaman <h.zzzaman@gmail.com>
2020-05-30 23:10:25 +0600

    money page fix

--- lib/pags/new_product.dart
+++ lib/pags/new_product.dart
@@ -26,125 +26,127 @@ class _NewProductState extends State<NewProduct> {
     double screenWidth = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          //main
-          height: screenHeight,
-          width: screenWidth,
-          child: Column(
-            children: <Widget>[
-              Container(
-                //produt details
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'All Products',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 0, 8, 5),
-                      child: Text('Details  product details  product details  product details  product details ',
-                        overflow: TextOverflow.visible,
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),),
-                    )
-                  ],
-                ),
-              ),
-              Divider(color: Colors.amber,height: 2,),
-              Container(
-                //Addnew product
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width * .9,
-                    child: FlatButton(
-                      color: Colors.amber,
-                      onPressed: (){},
-                      child: Center(
-                        child: Row(
-                          children: <Widget>[
-                            SizedBox(width: 70,),
-                            Icon(
-                              Icons.add
-                            ),
-                            Text(
-                              'Add A New Product'
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                //Category
-                width: screenWidth,
-                height: screenHeight * .12,
-//                  color: Colors.red,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8,left: 23),
-                      child: Text(
-                        'Catergory',
-                        style: TextStyle(
-                          fontSize: 15
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 3,),
-                    Center(
-                      child: DottedBorder(
-                        strokeWidth: .5,
-                        color: Colors.black,
-                        child: Padding(
-                          padding: const EdgeInsets.all(3.0),
-                          child: Container(
-                            width: screenWidth - 50,
-                            child: DropdownButton(
-                              elevation: 0,
-                              hint: Text('Please Choose a Category'),
-                              value: _dropSelectedItem,
-                              onChanged: (val){
-                                setState(() {
-                                  _dropSelectedItem = val;
-                                });
-                              },
-                              items: dropdownItem.map((item){
-                                return DropdownMenuItem(
-                                  child: new Text(item),
-                                  value: item,
-                                );
-                              }).toList(),
-                            ),
+        body: SingleChildScrollView(
+          child: Container(
+            //main
+            height: double.infinity,
+            width: double.infinity,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  //produt details
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'All Products',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20
                           ),
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: stockItems.map((item){
-                      return CustomTile(stockModel: item);
-                    }).toList(),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 0, 8, 5),
+                        child: Text('Details  product details  product details  product details  product details ',
+                          overflow: TextOverflow.visible,
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),),
+                      )
+                    ],
                   ),
                 ),
-              )
-            ],
+                Divider(color: Colors.amber,height: 2,),
+                Container(
+                  //Addnew product
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width * .9,
+                      child: FlatButton(
+                        color: Colors.amber,
+                        onPressed: (){},
+                        child: Center(
+                          child: Row(
+                            children: <Widget>[
+                              SizedBox(width: 70,),
+                              Icon(
+                                Icons.add
+                              ),
+                              Text(
+                                'Add A New Product'
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  //Category
+                  width: screenWidth,
+                  height: screenHeight * .12,
+//                  color: Colors.red,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8,left: 23),
+                        child: Text(
+                          'Category',
+                          style: TextStyle(
+                            fontSize: 15
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 3,),
+                      Center(
+                        child: DottedBorder(
+                          strokeWidth: .5,
+                          color: Colors.black,
+                          child: Padding(
+                            padding: const EdgeInsets.all(3.0),
+                            child: Container(
+                              width: screenWidth - 50,
+                              child: DropdownButton(
+                                elevation: 0,
+                                hint: Text('Please Choose a Category'),
+                                value: _dropSelectedItem,
+                                onChanged: (_){
+                                  setState(() {
+                                    _dropSelectedItem = _;
+                                  });
+                                },
+                                items: dropdownItem.map((item){
+                                  return DropdownMenuItem(
+                                    child: new Text(item),
+                                    value: item,
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: stockItems.map((item){
+                        return CustomTile(stockModel: item);
+                      }).toList(),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         )
       ),
